d85504c18836e3c41c7ab943a33753d942cbcba3
SKuvic <86663907+SKuvic@users.noreply.github.com>
2026-06-12 15:23:47 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    int usr_num;
+
+    std::cout << "Введите число:\n";
+    std::cin >> usr_num;
+    std::cout << "Вы ввели:\n" << usr_num;
+  
+    return 0;
+}
---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    string usr_word;
+
+    std::cout << "Введите слово: \n";
+    std::cin >> usr_word;
+    std::cout << "Вы ввели:\n"<< usr_word;
+
+    return 0;
+}
