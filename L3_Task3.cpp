a6be4746b9426d3ba6b14e61d4f2e9bf156bac5c
SKuvic <86663907+SKuvic@users.noreply.github.com>
2026-06-21 19:33:16 +0300

    Add files via upload

---
+++ L3_Task3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+int main () {
+
+    std::string Gender;
+    int Age;
+    std::string  Zodiac;
+
+
+    std::cout << "Введите пол: ";
+    std::cin >> Gender;
+    std::cout << "Введите знак зодиака: ";
+    std::cin >> Zodiac;
+    std::cout << "Введите возраст: ";
+    std::cin >> Age;
+
+
+    //Анкета для проверки входных
+    std::cout << "Анкета" << std::endl;
+    std::cout << "Ваш пол: " << Gender << std::endl;
+    std::cout << "Ваш знак зодиака: " << Zodiac << std::endl;
+    std::cout << "Ваш возраст: " << Age << std::endl;
+
+
+
+
+    if ((Gender == "м") && (Age < 40) && (Zodiac == "рыбы" || Zodiac == "скорпион" || Zodiac == "рак"))
+    {
+        std::cout << "Сегодня очень плодотворный день.\n"
+            "Можно добиться того, что прежде казалось почти невозможным." << std::endl;
+
+    }
+    else if ((Gender == "ж") && (15 <= Age <= 50) && (Zodiac == "козерог" || Zodiac == "дева" || Zodiac == "телец"))
+    {
+        std::cout << "Сегодняшний вечер подходит для общения с друзьями, проведения домашних праздников и импровизированных вечеринок. \n"
+            "Будет не только весело, но и интересно : найдётся дело, которое увлечёт всех." << std::endl;
+
+    }
+    else
+    std::cout << "Гороскоп для вас находится в разработке.Приходите чуточку позже;)\n" << std::endl;
+
+    return 0;
+
+}
